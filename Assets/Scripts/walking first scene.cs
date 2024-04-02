c9cb88ab06eb90c67c506d46e1e12f2eb8d3e8fb
sarawashere <58453206+sarawashere@users.noreply.github.com>
2024-04-02 13:34:48 -0400

    first walk

--- Assets/Scripts/walking first scene.cs
+++ Assets/Scripts/walking first scene.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class walkingfirstscene : MonoBehaviour
 {
     private float speed;
+    public float start;
+    public float stop;
+
+    public TextMeshPro endText;
+
+    private float pos;
+    private bool gameOver = false;
+    private float tree1 = -8.7f;
+    private int part = 1;
+    private bool space = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +31,25 @@ public class walkingfirstscene : MonoBehaviour
         }
         if(Input.GetKey(KeyCode.DownArrow)){
             transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+        }
+        
+        if(curr >= tree1 && part == 1)
+        {
+            endText.text = "Press 'SPACE' to\n  jump over the log";
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                endText.text = "";
+                transform.position = new Vector3(0f, 1f, -8.3f);
+                startLocation = -8.3f;                    
+                part=2;
+            }
+            else if(Input.GetKey(KeyCode.DownArrow))
+            {
+                endText.text = "";
+                if(currPosition > startLocation){
+                    transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+                }
+            }
         }
     }
 }
