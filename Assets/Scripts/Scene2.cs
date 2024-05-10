4325fb160f87521c6f167a228e8688594d7e8deb
iakala <iakala@umd.edu>
2024-05-10 03:13:50 -0400

    updates

---
+++ Assets/Scripts/Scene2.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene2 : MonoBehaviour
+{
+    public float triggerXPosition; // The x position at which to trigger the scene transition
+
+    void Update()
+    {
+        // Check if the player has passed the specified x position
+        if (transform.position.x <= triggerXPosition)
+        {
+            // Load the next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
