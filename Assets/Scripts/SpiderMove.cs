1515fbfd8dd00a5cab2521bf09a54dc13dfe3c79
iakala <iakala@umd.edu>
2024-05-10 03:41:24 -0400

    updates

--- Assets/Scripts/SpiderMove.cs
+++ Assets/Scripts/SpiderMove.cs
@@ -5,31 +5,26 @@ using UnityEngine;
 public class SpiderMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float changeInterval = 2f; // Time between direction changes
-    public float forceStrength = 10f; // Strength of the force
+    public Animator animator;
+    public float moveSpeed = 5f;
+    public float frequency = 1f;  // Speed of oscillation
+    public float magnitude = 5f;  // Distance of oscillation
+    private Vector3 startPos;
     private Rigidbody rb;
-    private float nextChangeTime;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        nextChangeTime = Time.time + changeInterval;
-        ApplyForce();
+        startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Time.time >= nextChangeTime)
-        {
-            nextChangeTime += changeInterval;
-            ApplyForce();
-        }
-    }
+        float move = Mathf.Sin(Time.time * frequency) * magnitude;
+        transform.position = startPos + new Vector3(move, 0, 0);
+        rb.velocity = new Vector3(move * moveSpeed, rb.velocity.y, rb.velocity.z);
 
-    void ApplyForce()
-    {
-        // Reverse the force direction
-        rb.velocity = Vector3.zero; // Reset velocity to prevent speed build-up
-        rb.AddForce(new Vector3(forceStrength, 0, 0) * (Time.time % (changeInterval * 2) < changeInterval ? 1 : -1), ForceMode.VelocityChange);
+        // Update animation parameters
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
     }
 }
